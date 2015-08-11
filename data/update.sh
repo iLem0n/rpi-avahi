84b3bfcf381127f278dd4acd06f371fa44053fa0
Nuno Sousa <nunofgs@gmail.com>
2015-08-11 23:15:12 +0100

    Fix update script

--- data/update.sh
+++ data/update.sh
@@ -4,7 +4,7 @@
 sed -i '/^\s*$/d' /opt/app/generated.service
 
 # Split file by xml root tag.
-csplit -z -s -b '%02d.service' -f '/etc/avahi/services/service-' /opt/app/generated.service '/<?xml version="1.0" standalone="no"?>/'
+csplit -z -s -b '%02d.service' -f '/etc/avahi/services/service-' /opt/app/generated.service '/<?xml version="1.0" standalone="no"?>/' {*}
 
 # Reload avahi daemon.
 avahi-daemon --reload
